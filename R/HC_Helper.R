47782cb8a37c2f57a7be2fdfb3142d6e00c717e1
paytonjm <105075331+paytonjm@users.noreply.github.com>
2026-09-16 16:20:02 -0700

    Update HC_Helper.R
    Added prompt call for custom map option, and set default to "FALSE", and then added ad=as.logical() to convert to TRUE or FALSE.

--- R/HC_Helper.R
+++ R/HC_Helper.R
@@ -21,7 +21,10 @@ OED_Export_HiChart <- function(
       "Enter chart number",
       default = "1"
     ),
-    .custom_map = FALSE
+    .custom_map = showPrompt(
+      "Is this a custom map?",
+      "Enter TRUE or FALSE",
+      default = "FALSE")
 ) {
 
   # ============================================================
@@ -32,7 +35,7 @@ OED_Export_HiChart <- function(
   title <- .title
   alt_text <- .alt_text
   chart_num <- .chart_num
-  custom_map <-  .custom_map
+  custom_map <-  as.logical(.custom_map)
 
   container_id_full <- paste0(
     container_id,
